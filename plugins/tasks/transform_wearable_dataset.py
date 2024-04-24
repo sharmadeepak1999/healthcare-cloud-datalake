a9c22560780e6058c6a3f6e69709575efb901930
Deepak Sharma <sharmadeepak1999@gmail.com>
2024-04-24 11:07:36 +0530

    updated transform for wearable dataset

--- plugins/tasks/transform_wearable_dataset.py
+++ plugins/tasks/transform_wearable_dataset.py
@@ -1,9 +1,7 @@
-import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 import os
-
 def transform_wearable_dataset(ti):
     df = ti.xcom_pull(task_ids='read_dataset', key='dataset')
-    import pandas as pd
 
     df.drop(columns=['ref', 'study_id'], inplace=True)
 
@@ -18,6 +16,15 @@ def transform_wearable_dataset(ti):
     df['handedness'] = df['handedness'].replace('left', 'LEFT')
     df['appearance_in_kinship'] = df['appearance_in_kinship'].replace('true', 'TRUE')
     df['appearance_in_kinship'] = df['appearance_in_kinship'].replace('false', 'FALSE')
+
+
+    le=LabelEncoder()
+    df['gender']=le.fit_transform(df['gender'])
+    df['condition']=le.fit_transform(df['condition'])
+    df['disease_comment']=le.fit_transform(df['disease_comment'])
+    df['handedness']=le.fit_transform(df['handedness'])
+    df['effect_of_alcohol_on_tremor']=le.fit_transform(df['effect_of_alcohol_on_tremor'])
+
 
     df.to_csv(os.path.join(os.environ.get('AIRFLOW_HOME'), "datasets/wearable-dataset/output/final_wearable_dataset.csv"), index=False)
     ti.xcom_push(key='transformed_dataset', value=df)
