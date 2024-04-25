cb2dfda291c17b3f12518d9cdb7f6c2d756a80be
Pranav-Rustagi <pranav001100@gmail.com>
2024-04-25 17:38:11 +0530

    Modified EHR data pipeline files

--- dags/electronic_health_record_dag.py
+++ dags/electronic_health_record_dag.py
@@ -48,7 +48,7 @@ def create_dag(dataset_file, bucket_name, description="Sample Description", dag_
 
     return dag
 
-dataset_file_path = 'datasets/electronic-health-record/input/sampled_electronic_health_records.csv'
+dataset_file_path = 'datasets/electronic-health-record/input/electronic_health_record_dataset.csv'
 s3_bucket_name = 'deepak-sample-850072525'
 
 dag = create_dag(
